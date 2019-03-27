42b569e3d663088c4fbec091eddb423aafae1f60
Jazeps Basko <jazeps.basko@gmail.com>
2019-03-27 22:56:08 +0000

    v1.0.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="strictus",
-    version="1.0.0",
+    version="1.0.1",
     url="https://github.com/jbasko/strictus",
     license="MIT",
     author="Jazeps Basko",
